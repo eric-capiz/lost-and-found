f9b99150bae86a0b96d8337a3d628ceab7d1e98b
eric capiz <ericcapiz@gmail.com>
2024-12-19 18:53:56 -0600

    frontend deployed

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "https://lost-and-found-rosy.vercel.app",
+      "https://lost-and-found-git-main-eric-capizs-projects.vercel.app",
+      "https://lost-and-found-a07korgs9-eric-capizs-projects.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
