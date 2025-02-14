1384f58754c8da694e3fc2510e972f1abce45f6a
eric capiz <ericcapiz@gmail.com>
2025-02-14 16:41:40 -0600

    fix bug in add post

--- frontend/src/components/modals/PostItem.jsx
+++ frontend/src/components/modals/PostItem.jsx
@@ -3,9 +3,11 @@ import { FiX } from "react-icons/fi";
 import { Spinner, Alert } from "../common";
 import { AuthContext } from "../../contexts/auth/AuthContext";
 import { usePosts } from "../../contexts/post/PostContext";
+import { postService } from "../../services/postService";
 
 const PostItem = ({ isOpen, onClose }) => {
   const { updateUserCounts } = useContext(AuthContext);
+  const { addPost } = usePosts();
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -74,6 +76,8 @@ const PostItem = ({ isOpen, onClose }) => {
       selectedImages.forEach((image) => {
         formDataToSend.append("images", image);
       });
+
+      await addPost(formDataToSend);
 
       await updateUserCounts("post", "create");
 
